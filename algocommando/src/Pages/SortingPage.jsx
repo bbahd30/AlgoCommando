b7e869540cc628154f91449fdddef98d1e83522b
bbahd30 <b_bonal@cs.iitr.ac.in>
2023-03-21 22:20:26 +0530

    Initialize sorting

--- algocommando/src/Pages/SortingPage.jsx
+++ algocommando/src/Pages/SortingPage.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react'
+import {useSelector} from 'react-redux'
+import {ReactP5Wrapper} from "react-p5-wrapper"
+import { Paper, Box } from '@mui/material'
 
-const SortingPage = () => {
+import SortingSketch from '../Animations/SortingSketch'
+
+const SortingPage = () =>
+{
+    const sortingState = useSelector((state) => state.sorting)
+    const animation = sortingState.animation
+    const sortType = sortingState.sortType
+    const array = sortingState.array
+    const speed = sortingState.speed
+
     return (
-        <div>
-            Sorting Page
-        </div>
-    );
-};
+        <>
+            <Paper>
+                <Box>
+                    Bubble Sort
+                </Box>
+                <Box>
+                    <ReactP5Wrapper sketch={SortingSketch} />
+                </Box>
+            </Paper>
+        </>
+        
+    )
+}
 
-export default SortingPage;
+export default SortingPage
