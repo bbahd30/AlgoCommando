6975b92e9f88964323de5201ac14aa547010bfe1
bbahd30 <b_bonal@cs.iitr.ac.in>
2023-04-17 15:22:42 +0530

    Switch to color change animation

--- algocommando/src/Animations/SortingSketch.js
+++ algocommando/src/Animations/SortingSketch.js
@@ -35,94 +35,94 @@ function sketch(p5, sortingState)
     let swapSpeed = 1
 
     // note:sorting canvas with double swap 
-    p5.draw = () =>
-    {
-        p5.background("#F9F9F9")
-        let firstBox, secondBox
-        let crossed
-        for (let i = 0; i < currentArray.length; i++)
-        {
-            crossed = false
-            let color = defaultColor
-            if (i <= traversedIndex) color = traversedColor
-            if (i === firstCorrected || i === secondCorrected) color = yellow
-            else if (i >= sorted && sorted != -1) color = sortedColor
-
-            const [height, x, y, z] = findDimensions(
-                i,
-                currentArray[i],
-                rectWidth,
-                heightFactor
-            )
-
-            if (i === firstIndex)
-                firstBox = { height, x, y, z }
-            else if (i === secondIndex)
-                secondBox = { height, x, y, z }
-            else
-            {
-                p5.push()
-                p5.translate(x, y, z)
-                p5.fill(color)
-                p5.box(rectWidth, height, rectDepth)
-                p5.pop()
-            }
-        }
-
-        if (firstBox && secondBox)
-        {
-            if (swapAnimation >= rectWidth)
-            {
-                firstBox = null
-                secondBox = null
-                swapAnimation = 0
-                crossed = true
-            }
-            else
-                swapAnimation += swapSpeed
-
-            if (!crossed)
-            {
-                p5.push()
-                p5.translate(firstBox.x + swapAnimation, firstBox.y, firstBox.z)
-                p5.fill(swappedColor)
-                p5.box(rectWidth, firstBox.height, rectDepth)
-                p5.pop()
-
-                p5.push()
-                p5.translate(secondBox.x - swapAnimation, secondBox.y, secondBox.z)
-                p5.fill(swappedColor)
-                p5.box(rectWidth, secondBox.height, rectDepth)
-                p5.pop()
-            }
-        }
-    }
-
-
-    // note:sorting canvas without animation
     // p5.draw = () =>
     // {
     //     p5.background("#F9F9F9")
-    //     for (let i = 0 i < currentArray.length i++)
+    //     let firstBox, secondBox
+    //     let crossed
+    //     for (let i = 0; i < currentArray.length; i++)
     //     {
+    //         crossed = false
     //         let color = defaultColor
-    //         if (i <= traversedIndex)
-    //             color = traversedColor
-    //         if (i === firstCorrected || i === secondCorrected)
-    //             color = yellow
-    //         else if (i === firstIndex || i === secondIndex)
-    //             color = swappedColor
-    //         if (i >= sorted && sorted != -1)
-    //             color = sortedColor
-    //         const [height, x, y, z] = findDimensions(i, currentArray[i], rectWidth, heightFactor)
+    //         if (i <= traversedIndex) color = traversedColor
+    //         if (i === firstCorrected || i === secondCorrected) color = yellow
+    //         else if (i >= sorted && sorted != -1) color = sortedColor
 
-    //         p5.push()
-    //         p5.translate(x, y, z)
-    //         p5.fill(color)
-    //         p5.box(rectWidth, height, rectDepth)
-    //         p5.pop()
+    //         const [height, x, y, z] = findDimensions(
+    //             i,
+    //             currentArray[i],
+    //             rectWidth,
+    //             heightFactor
+    //         )
+
+    //         if (i === firstIndex)
+    //             firstBox = { height, x, y, z }
+    //         else if (i === secondIndex)
+    //             secondBox = { height, x, y, z }
+    //         else
+    //         {
+    //             p5.push()
+    //             p5.translate(x, y, z)
+    //             p5.fill(color)
+    //             p5.box(rectWidth, height, rectDepth)
+    //             p5.pop()
+    //         }
+    //     }
+
+    //     if (firstBox && secondBox)
+    //     {
+    //         if (swapAnimation >= rectWidth)
+    //         {
+    //             firstBox = null
+    //             secondBox = null
+    //             swapAnimation = 0
+    //             crossed = true
+    //         }
+    //         else
+    //             swapAnimation += swapSpeed
+
+    //         if (!crossed)
+    //         {
+    //             p5.push()
+    //             p5.translate(firstBox.x + swapAnimation, firstBox.y, firstBox.z)
+    //             p5.fill(swappedColor)
+    //             p5.box(rectWidth, firstBox.height, rectDepth)
+    //             p5.pop()
+
+    //             p5.push()
+    //             p5.translate(secondBox.x - swapAnimation, secondBox.y, secondBox.z)
+    //             p5.fill(swappedColor)
+    //             p5.box(rectWidth, secondBox.height, rectDepth)
+    //             p5.pop()
+    //         }
     //     }
     // }
+
+
+    // note:sorting canvas without animation
+    p5.draw = () =>
+    {
+        p5.background("#F9F9F9")
+        for (let i = 0; i < currentArray.length; i++)
+        {
+            let color = defaultColor
+            if (i <= traversedIndex)
+                color = traversedColor
+            if (i === firstCorrected || i === secondCorrected)
+                color = yellow
+            else if (i === firstIndex || i === secondIndex)
+                color = swappedColor
+            if (i >= sorted && sorted != -1)
+                color = sortedColor
+            const [height, x, y, z] = findDimensions(i, currentArray[i], rectWidth, heightFactor)
+
+            p5.push()
+            p5.translate(x, y, z)
+            p5.fill(color)
+            p5.box(rectWidth, height, rectDepth)
+            p5.pop()
+        }
+    }
 
     if (animation)
         bubbleSort(currentArray, (newArray) => currentArray = [...newArray])
